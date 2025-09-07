e0ce49706947ee0cf47fe4e51ebca7d3c428e7e1
einviolaspieler-tech <einviolaspieler@gmail.com>
2025-09-07 14:58:21 +0900

    Create characters.js

---
+++ js/characters.js
@@ -0,0 +1,7 @@
+export const CHARACTERS = {
+  char01: { name:'FLARE', special:{ cost:[34,67,100], id:'flare' } },
+  char02: { name:'AQUA',  special:{ cost:[40,70,100], id:'aqua' } },
+  char03: { name:'BLADE', special:{ cost:[30,60,100], id:'blade' } },
+  char04: { name:'NOVA',  special:{ cost:[35,65,100], id:'nova' } },
+  char05: { name:'GAIA',  special:{ cost:[25,55,100], id:'gaia' } },
+};
